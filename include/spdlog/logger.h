8c5e917ccc5a342b13ade546cc73fbd88e851a96
Alexey Biryukov <alexey.biryukov@ibm.com>
2025-02-26 14:04:45 -0500

    Allocator aware sink vector
    Sinks are stored in basic_logger using the allocator data type now.
    
    Test coverage checking that sink vector allocations are made with proper allocator.

--- include/spdlog/logger.h
+++ include/spdlog/logger.h
@@ -57,13 +57,17 @@ public:
 
     using string_type = std::basic_string<char, std::char_traits<char>, Alloc>;
 
+    template <typename T>
+    using vector_type =
+        std::vector<T, typename std::allocator_traits<Alloc>::template rebind_alloc<T>>;
+
     // Empty logger
     explicit basic_logger(string_type name,
                           Alloc alloc_fmt_buf = Alloc(),
                           Alloc alloc_data = Alloc())
         : Alloc(alloc_fmt_buf),
           name_(std::move(name), alloc_data),
-          sinks_() {}
+          sinks_(alloc_data) {}
 
     // Logger with range on sinks
     template <typename It>
@@ -74,7 +78,16 @@ public:
                  Alloc alloc_data = Alloc())
         : Alloc(alloc_fmt_buf),
           name_(std::move(name), alloc_data),
-          sinks_(begin, end) {}
+          sinks_(begin, end, alloc_data) {}
+
+    // Logger with sinks in a vector
+    basic_logger(string_type name,
+                 vector_type<sink_ptr<Alloc>> sinks,
+                 Alloc alloc_fmt_buf = Alloc(),
+                 Alloc alloc_data = Alloc())
+        : Alloc(alloc_fmt_buf),
+          name_(std::move(name), alloc_data),
+          sinks_(std::move(sinks), alloc_data) {}
 
     // Logger with single sink
     basic_logger(string_type name,
@@ -318,9 +331,9 @@ public:
     level::level_enum flush_level() const;
 
     // sinks
-    const std::vector<sink_ptr<Alloc>> &sinks() const;
+    const vector_type<sink_ptr<Alloc>> &sinks() const;
 
-    std::vector<sink_ptr<Alloc>> &sinks();
+    vector_type<sink_ptr<Alloc>> &sinks();
 
     // error handler
     void set_error_handler(err_handler);
@@ -330,7 +343,7 @@ public:
 
 protected:
     string_type name_;
-    std::vector<sink_ptr<Alloc>> sinks_;
+    vector_type<sink_ptr<Alloc>> sinks_;
     spdlog::level_t level_{level::info};
     spdlog::level_t flush_level_{level::off};
     err_handler custom_err_handler_{nullptr};
